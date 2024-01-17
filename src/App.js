09f48a91d8cca8465560a6c67b216fbe61827078
cxnvti <jenishbekov24bj@gmail.com>
2024-01-17 10:37:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ColorToggleSquare = () => {
+  const [isRed, setIsRed] = useState(true);
+
+  const toggleColor = () => {
+    setIsRed((prevIsRed) => !prevIsRed);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          width: '300px',
+          height: '300px',
+          margin: '0 auto',
+          backgroundColor: isRed ? 'red' : 'blue',
+          cursor: 'pointer',
+        }}
+        onClick={toggleColor}
+      />
+      <div
+        style={{
+          width: '300px',
+          height: '300px',
+          margin: '0 auto',
+          backgroundColor: isRed ? 'blue' : 'red',
+          cursor: 'pointer',
+        }}
+        onClick={toggleColor}
+      />
     </div>
   );
-}
+};
 
-export default App;
+export default ColorToggleSquare;
